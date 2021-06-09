e39473761d731b2c9728810de871bdca6d46091b
yinchunyu <542968439@qq.com>
2021-06-09 10:24:05 +0800

    first commit

---
+++ components/table/SelectionBox.jsx
@@ -0,0 +1,64 @@
+import Checkbox from '../checkbox';
+import Radio from '../radio';
+import { SelectionBoxProps } from './interface';
+import BaseMixin from '../_util/BaseMixin';
+import { getOptionProps, getListeners } from '../_util/props-util';
+
+export default {
+  name: 'SelectionBox',
+  mixins: [BaseMixin],
+  props: SelectionBoxProps,
+  data() {
+    return {
+      checked: this.getCheckState(this.$props),
+    };
+  },
+
+  mounted() {
+    this.subscribe();
+  },
+
+  beforeDestroy() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  },
+  methods: {
+    getCheckState(props) {
+      const { store, defaultSelection, rowIndex } = props;
+      let checked = false;
+      if (store.getState().selectionDirty) {
+        checked = store.getState().selectedRowKeys.indexOf(rowIndex) >= 0;
+      } else {
+        checked =
+          store.getState().selectedRowKeys.indexOf(rowIndex) >= 0 ||
+          defaultSelection.indexOf(rowIndex) >= 0;
+      }
+      return checked;
+    },
+    subscribe() {
+      const { store } = this;
+      this.unsubscribe = store.subscribe(() => {
+        const checked = this.getCheckState(this.$props);
+        this.setState({ checked });
+      });
+    },
+  },
+
+  render() {
+    const { type, rowIndex, ...rest } = getOptionProps(this);
+    const { checked } = this;
+    const checkboxProps = {
+      props: {
+        checked,
+        ...rest,
+      },
+      on: getListeners(this),
+    };
+    if (type === 'radio') {
+      checkboxProps.props.value = rowIndex;
+      return <Radio {...checkboxProps} />;
+    }
+    return <Checkbox {...checkboxProps} />;
+  },
+};
---
+++ components/vc-table/src/TableHeaderRow.jsx
@@ -0,0 +1,98 @@
+import classNames from 'classnames';
+import PropTypes from '../../_util/vue-types';
+import { connect } from '../../_util/store';
+import { mergeProps } from '../../_util/props-util';
+
+const TableHeaderRow = {
+  props: {
+    index: PropTypes.number,
+    fixed: PropTypes.string,
+    columns: PropTypes.array,
+    rows: PropTypes.array,
+    row: PropTypes.array,
+    components: PropTypes.object,
+    height: PropTypes.any,
+    customHeaderRow: PropTypes.func,
+    prefixCls: PropTypes.prefixCls,
+  },
+  name: 'TableHeaderRow',
+  render(h) {
+    const { row, index, height, components, customHeaderRow, prefixCls } = this;
+    const HeaderRow = components.header.row;
+    const HeaderCell = components.header.cell;
+    const rowProps = customHeaderRow(
+      row.map(cell => cell.column),
+      index,
+    );
+    const customStyle = rowProps ? rowProps.style : {};
+    const style = { height, ...customStyle };
+    if (style.height === null) {
+      delete style.height;
+    }
+    return (
+      <HeaderRow {...rowProps} style={style}>
+        {row.map((cell, i) => {
+          const { column, isLast, children, className, ...cellProps } = cell;
+          const customProps = column.customHeaderCell ? column.customHeaderCell(column) : {};
+          const headerCellProps = mergeProps(
+            {
+              attrs: {
+                ...cellProps,
+              },
+            },
+            {
+              ...customProps,
+              key: column.key || column.dataIndex || i,
+            },
+          );
+
+          if (column.align) {
+            headerCellProps.style = { ...customProps.style, textAlign: column.align };
+          }
+
+          headerCellProps.class = classNames(
+            customProps.class,
+            customProps.className,
+            column.class,
+            column.className,
+            {
+              [`${prefixCls}-align-${column.align}`]: !!column.align,
+              [`${prefixCls}-row-cell-ellipsis`]: !!column.ellipsis,
+              [`${prefixCls}-row-cell-break-word`]: !!column.width,
+              [`${prefixCls}-row-cell-last`]: isLast,
+            },
+          );
+
+          if (typeof HeaderCell === 'function') {
+            return HeaderCell(h, headerCellProps, children);
+          }
+          return <HeaderCell {...headerCellProps}>{children}</HeaderCell>;
+        })}
+      </HeaderRow>
+    );
+  },
+};
+
+function getRowHeight(state, props) {
+  const { fixedColumnsHeadRowsHeight } = state;
+  const { columns, rows, fixed } = props;
+  const headerHeight = fixedColumnsHeadRowsHeight[0];
+
+  if (!fixed) {
+    return null;
+  }
+
+  if (headerHeight && columns) {
+    if (headerHeight === 'auto') {
+      return 'auto';
+    }
+    return `${headerHeight / rows.length}px`;
+  }
+  return null;
+}
+
+export default connect((state, props) => {
+  return {
+    height: getRowHeight(state, props),
+  };
+})(TableHeaderRow);
